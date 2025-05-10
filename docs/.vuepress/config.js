fb74b7f584688789169a78dec2c57019748085fc
Weegoos <batyr.ashim05@mail.ru>
2025-05-10 17:14:42 +0500

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'Guides',
+  description: 'Подсказки для программирования',
+
+  theme: defaultTheme({
+    logo: '/images/docs/code_logo.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
